69552429e962c6d4b119b5683c18aed53811d315
unknown <foxaki@gmail.com>
2020-09-21 15:57:02 +0800

    專案啟動

---
+++ ledblink.ino
@@ -0,0 +1,22 @@
+void setup() {
+  pinMode(13, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+  delay(20);
+  digitalWrite(13, LOW);
+  delay(20);
+  digitalWrite(13, HIGH);
+  delay(50);
+  digitalWrite(13, LOW);
+  delay(50);
+  digitalWrite(13, HIGH);
+  delay(80);
+  digitalWrite(13, LOW);
+  delay(80);
+
+
+
+}
