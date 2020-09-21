0e185e9fa3e84609b3bc4bcd62b9fc9572b05e68
foxaki <foxaki@gmail.com>
2020-09-21 16:30:24 +0800

    20->200

--- ledblink.ino
+++ ledblink.ino
@@ -5,17 +5,10 @@ void setup() {
 
 void loop() {
   digitalWrite(13, HIGH);
-  delay(20);
+  delay(200);
   digitalWrite(13, LOW);
-  delay(20);
-  digitalWrite(13, HIGH);
-  delay(50);
-  digitalWrite(13, LOW);
-  delay(50);
-  digitalWrite(13, HIGH);
-  delay(80);
-  digitalWrite(13, LOW);
-  delay(80);
+  delay(200);
+ 
 
 
 
